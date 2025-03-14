65c718ec4ba6b318b6fd29277fb9d4a387760c78
imfakebot <115884438+imfakebot@users.noreply.github.com>
2025-03-14 22:26:06 +0700

    44

--- script.js
+++ script.js
@@ -2,20 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
     let selectBox = document.getElementById("memberSelect");
     let memberList = document.getElementById("memberList");
 
-
     selectBox.addEventListener("mouseenter", function () {
-        memberList.classList.remove("hidden");
+        selectBox.style.display = "none";
+        memberList.classList.remove("hidden"); 
     });
-
-
-    selectBox.addEventListener("mouseleave", function () {
-        setTimeout(() => {
-            memberList.classList.add("hidden");
-        }, 500); 
-    });
-
-  
     memberList.addEventListener("mouseleave", function () {
-        memberList.classList.add("hidden");
+        memberList.classList.add("hidden"); 
+        selectBox.style.display = "block"; 
     });
 });
